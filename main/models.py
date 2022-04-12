beb264fa1867cfec3a55dc9f5a60be7f4eec69a5
Kevin Joshi <kevinjoshi46b@gmail.com>
2022-04-13 00:02:18 +0530

    feat: created the model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+    question = models.TextField()
+
+    def __str__(self) -> str:
+        return self.question
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.TextField()
+
+    def __str__(self) -> str:
+        return self.answer
+
+
+class AnswerUpvote(models.Model):
+    question = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
